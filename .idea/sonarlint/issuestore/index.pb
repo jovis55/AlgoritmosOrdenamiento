eaba40f8d27a2d5262100827b30b3fb3d92faee7
johana <johanapaolapalacio@gmail.com>
2024-10-09 20:57:47 -0500

    Algoritmos

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
 S
 #src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+Y
+)src/main/java/org/example/algoritmos.java,8\8\88d8c9c126117081c0dd624ff41443d52e55bac7
---
+++ src/main/java/org/example/algoritmos.java
@@ -0,0 +1,152 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class algoritmos {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Intercambiar arr[j] y arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void stoogeSort(int[] arr, int l, int h) {
+        if (l >= h)
+            return;
+
+        if (arr[l] > arr[h]) {
+            int temp = arr[l];
+            arr[l] = arr[h];
+            arr[h] = temp;
+        }
+
+        if (h - l + 1 > 2) {
+            int t = (h - l + 1) / 3;
+            stoogeSort(arr, l, h - t);
+            stoogeSort(arr, l + t, h);
+            stoogeSort(arr, l, h - t);
+        }
+    }
+
+    public static void pigeonholeSort(int[] arr) {
+        int min = Arrays.stream(arr).min().getAsInt();
+        int max = Arrays.stream(arr).max().getAsInt();
+        int range = max - min + 1;
+        int[] holes = new int[range];
+
+        for (int i : arr) {
+            holes[i - min]++;
+        }
+
+        int index = 0;
+        for (int i = 0; i < range; i++) {
+            while (holes[i]-- > 0) {
+                arr[index++] = i + min;
+            }
+        }
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        System.arraycopy(arr, l, L, 0, n1);
+        System.arraycopy(arr, m + 1, R, 0, n2);
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void bitonicSort(int[] arr, int low, int cnt, boolean dir) {
+        if (cnt > 1) {
+            int k = cnt / 2;
+            bitonicSort(arr, low, k, true);
+            bitonicSort(arr, low + k, k, false);
+            bitonicMerge(arr, low, cnt, dir);
+        }
+    }
+
+    private static void bitonicMerge(int[] arr, int low, int cnt, boolean dir) {
+        if (cnt > 1) {
+            int k = cnt / 2;
+            for (int i = low; i < low + k; i++) {
+                if (dir == (arr[i] > arr[i + k])) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + k];
+                    arr[i + k] = temp;
+                }
+            }
+            bitonicMerge(arr, low, k, dir);
+            bitonicMerge(arr, low + k, k, dir);
+        }
+    }
+
+
+
+}
